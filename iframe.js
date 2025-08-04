a694cf19d52368d619d68faaf939b4cb4d2d9929
Ben Bavar <ben.bavar@gmail.com>
2025-08-04 09:27:07 -0400

    Associate with each key in Shelf.#genreToBooks the entire array of shelved books that fit the genre the key names, not just one shelved book. Map book indices to the iframes containing the corresponding books' pages. Implement a method that removes the iframe corresponding to book of index i. Remove iframe once you are done setting the genres for that book. Recursively test each iframe's receptiveness and responsiveness to messages sent by parent page via postMessage. Once the test is passed, trigger a click event on the page in the iframe to reveal any hidden genres.

--- iframe.js
+++ iframe.js
@@ -1,40 +1,81 @@
+const sendGenresToParentPage = (genresList, eventData) => {
+  const genreSpans = genresList.querySelectorAll(
+    'span > span.BookPageMetadataSection__genreButton > a.Button > span.Button__labelItem'
+  );
+  const genres = [];
+  for (const span of genreSpans) {
+    // if (eventData.bookIndex === 1) {
+    //   console.log(
+    //     `Course in General Linguistics falls under the genre ${span.textContent}`
+    //   );
+    // }
+
+    // console.log(
+    //   `Book ${eventData.bookIndex} falls under the genre ${span.textContent}`
+    // );
+    genres.push(span.textContent);
+  }
+  window.parent.postMessage({
+    genres: genres,
+    bookIndex: eventData.bookIndex,
+  });
+};
+
 window.addEventListener('message', (event) => {
+  //   console.log(
+  //     `event.data.bookIndex = ${event.data.bookIndex}, event.origin = ${event.origin}`
+  //   );
   if (event.origin !== 'https://www.goodreads.com') {
     return;
   }
 
-  if (
-    event.data !== null &&
-    event.data.iframeModification === 'clickToShowMoreGenres'
-  ) {
-    const genresList = document.querySelector(
-      'div[data-testid="genresList"] > ul[aria-label="Top genres for this book"].CollapsableList'
-    );
+  if (event.data !== null) {
+    if ('string' in event.data && event.data.string === 'testing') {
+      window.parent.postMessage({
+        string: 'test passed',
+        bookIndex: event.data.bookIndex,
+      });
+    }
 
-    const hiddenGenresSublist = genresList.children[1];
+    if (event.data.iframeModification === 'clickToShowMoreGenres') {
+      // console.log(`event.data.bookIndex = ${event.data.bookIndex}`);
 
-    const moreGenresButton = genresList.querySelector(
-      'div.Button__container > button[aria-label="Show all items in the list"]'
-    );
-    moreGenresButton.click();
+      const genresList = document.querySelector(
+        'div[data-testid="genresList"] > ul[aria-label="Top genres for this book"].CollapsableList'
+      );
+      const moreGenresButton = genresList.querySelector(
+        'div.Button__container > button[aria-label="Show all items in the list"]'
+      );
+      //   console.log(
+      //     `On book ${event.data.bookIndex}'s page: moreGenresButton = ${moreGenresButton}`
+      //   );
+      if (moreGenresButton !== null) {
+        moreGenresButton.click();
 
-    const waitToGetGenres = () => {
-      if (hiddenGenresSublist.children.length === 0) {
-        setTimeout(waitToGetGenres, 100);
+        const hiddenGenresSublist = genresList.children[1];
+        const waitToGetGenres = () => {
+          if (hiddenGenresSublist.children.length === 0) {
+            setTimeout(waitToGetGenres, 100);
+          } else {
+            //   if (event.data.bookIndex === 1) {
+            //     console.log(
+            //       `sending genres of Course in General Linguistics to parent`
+            //     );
+            //   }
+
+            // console.log(
+            //   `waited; now sending genres of book ${event.data.bookIndex} to parent`
+            // );
+            sendGenresToParentPage(genresList, event.data);
+          }
+        };
+        waitToGetGenres();
       } else {
-        const genreSpans = genresList.querySelectorAll(
-          'span > span.BookPageMetadataSection__genreButton > a.Button > span.Button__labelItem'
-        );
-        const genres = [];
-        for (const span of genreSpans) {
-          genres.push(span.textContent);
-        }
-        window.parent.postMessage({
-          genres: genres,
-          bookIndex: event.data.bookIndex,
-        });
+        // console.log(
+        //   `no wait; sending genres of book ${event.data.bookIndex} to parent`
+        // );
+        sendGenresToParentPage(genresList, event.data);
       }
-    };
-    waitToGetGenres();
+    }
   }
 });
--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 class Shelf {
   #books = null;
-  #genresToBooks = new Map();
+  #testStatuses = [];
+  //   #iframes = [];
+  //   #bookToIframe = new Map();
+  #bookIndexToIframe = new Map();
+  #genreToBooks = new Map();
   #bookSelector = '#booksBody > tr.bookalike';
 
   getBooks() {
@@ -11,16 +15,52 @@ class Shelf {
     this.#books = document.querySelectorAll(this.#bookSelector);
   }
 
-  getGenresToBooks() {
-    return this.#genresToBooks;
+  fillTestStatuses() {
+    for (let i = 0; i < this.#books.length; i++) {
+      this.#testStatuses.push(0);
+    }
   }
 
-  async setGenresToBooks() {
+  //   getTestStatus() {
+
+  //   }
+
+  updateTestStatus(i) {
+    this.#testStatuses[i] = 1;
+  }
+
+  removeIframe(i) {
+    // this.#iframes[i].remove();
+    // this.#iframes.splice(i, 1);
+    // console.log(
+    //   `removing ${this.#books[i]
+    //     .querySelector('td.field.title > div.value > a')
+    //     .textContent.trim()} iframe`
+    // );
+    const iframe = this.#bookIndexToIframe.get(i);
+    iframe.remove();
+    this.#bookIndexToIframe.delete(i);
+  }
+
+  //   getBookToIframe() {
+  //     return this.#bookToIframe;
+  //   }
+
+  getBookIndexToIframe() {
+    return this.#bookIndexToIframe;
+  }
+
+  getGenreToBooks() {
+    return this.#genreToBooks;
+  }
+
+  async setGenreToBooks() {
     for (let i = 0; i < this.#books.length; i++) {
-      console.log(`i = ${i}`);
       const href = this.#books[i].querySelector(
         'td.field.title > div.value > a'
       ).href;
+
+      //   console.log(`i = ${i}, href = ${href}`);
 
       const iframe = document.createElement('iframe');
       document.body.insertBefore(
@@ -33,29 +73,54 @@ class Shelf {
       iframe.style.border = 0;
       iframe.src = href;
 
-      setTimeout(() => {
-        console.log(`attempting post, now that 5 seconds have passed`);
-        iframe.contentWindow.postMessage({
-          bookIndex: i,
-          iframeModification: 'clickToShowMoreGenres',
-        });
-      }, 5000);
+      //   this.#iframes.push(iframe);
+      //   this.#bookToIframe.set(this.#books[i], iframe);
+      this.#bookIndexToIframe.set(i, iframe);
 
-      //   break;
-      //   iframe.remove();
+      //   let testPassed = false;
+      const waitToPostMessage = () => {
+        // if (!testPassed) {
+        if (this.#testStatuses[i] === 0) {
+          iframe.contentWindow.postMessage({ string: 'testing', bookIndex: i });
+          setTimeout(waitToPostMessage, 100);
+        } else {
+          iframe.contentWindow.postMessage({
+            bookIndex: i,
+            iframeModification: 'clickToShowMoreGenres',
+          });
+        }
+      };
+      waitToPostMessage();
     }
+
+    // setTimeout(() => {
+    //   for (const genre of this.#genreToBooks.keys()) {
+    //     const books = this.#genreToBooks.get(genre);
+    //     console.log(`${genre} books on your shelf:`);
+    //     for (const book of books) {
+    //       console.log(
+    //         book.querySelector('td.field.title > div.value > a').textContent
+    //       );
+    //     }
+    //   }
+    // }, 15000);
   }
 
-  setGenresForBook(genres, bookIndex) {
+  setGenresForBook(genres, i) {
     for (const genre of genres) {
-      this.#genresToBooks.set(genre, this.#books[bookIndex]);
-      console.log(
-        `this.#genresToBooks.get(${genre}).querySelector('td.field.title > div.value > a').textContent = ${
-          this.#genresToBooks
-            .get(genre)
-            .querySelector('td.field.title > div.value > a').textContent
-        }`
-      );
+      if (this.#genreToBooks.has(genre)) {
+        this.#genreToBooks.get(genre).push(this.#books[i]);
+      } else {
+        this.#genreToBooks.set(genre, [this.#books[i]]);
+      }
+
+      //   const books = this.#genreToBooks.get(genre);
+      //   console.log(`${genre} books on your shelf:`);
+      //   for (const book of books) {
+      //     console.log(
+      //       book.querySelector('td.field.title > div.value > a').textContent
+      //     );
+      //   }
     }
   }
 }
@@ -64,12 +129,17 @@ const startUp = () => {
   const shelf = new Shelf();
 
   window.addEventListener('message', (event) => {
-    if (
-      event.data !== null &&
-      'genres' in event.data &&
-      'bookIndex' in event.data
-    ) {
-      shelf.setGenresForBook(event.data.genres, event.data.bookIndex);
+    if (event.data !== null) {
+      const i = event.data.bookIndex;
+
+      if ('string' in event.data && event.data.string === 'test passed') {
+        shelf.updateTestStatus(i);
+      }
+
+      if ('genres' in event.data && 'bookIndex' in event.data) {
+        shelf.setGenresForBook(event.data.genres, i);
+        shelf.removeIframe(i);
+      }
     }
   });
 
@@ -80,7 +150,8 @@ const startUp = () => {
         waitToGetGenres();
       }, 100);
     } else {
-      shelf.setGenresToBooks();
+      shelf.fillTestStatuses();
+      shelf.setGenreToBooks();
     }
   };
   waitToGetGenres();
